390d276deca50f27c8601734d4b1631afa3a1a87
SebasUr <sebasurberu1@gmail.com>
2025-05-07 16:31:44 -0500

    model(feat): added runs and data validation

--- src/train.py
+++ src/train.py
@@ -1,10 +1,9 @@
 # This is intended to be running in a cluster environment with at least 1 GPU and 16 cores.
-
-DATA_ROOT = "/home/sauriber2/cocoa/COCOA_DATASET_CLEAN"
+DATA_ROOT = "../data/COCOA_DATASET_CLEAN"
 DATA_YAML = os.path.join(DATA_ROOT, "data.yaml")
 
 # 1) Load pre-trained model, e.g., YOLOv8n, yolo11n.pt
-model = YOLO("yolo11x.pt")  
+model = YOLO("yolo11x.pt")
 
 # 2) Train
 model.train(
